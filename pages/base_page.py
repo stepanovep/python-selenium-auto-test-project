a4e286107f33972cdfd05d7883f4b1515240053b
stepanovep <stepanovep@gmail.com>
2019-09-10 00:49:00 +0300

    MainPage class implemented

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,20 @@
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class BasePage:
 
-    def __init__(self, browser: WebDriver, url):
+    def __init__(self, browser: WebDriver, url, timeout=5):
         self.browser = browser
+        self.browser.implicitly_wait(timeout)
         self.url = url
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
---
+++ pages/main_page.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+from .base_page import BasePage
+
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+
+        login_link = self.browser.find_element_by_css_selector('#login_link')
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, '#login_link')
